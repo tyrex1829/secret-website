659e2e96b9e9b32f12c77ef40f02ab87e2a07315
Saksham Khare <sakshamkhare19@gmail.com>
2024-03-10 10:00:05 +0530

    complete project

---
+++ index.js
@@ -0,0 +1,44 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url"
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+var userPassword = "Tyrex69_Gaming"
+var userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+function passwordChecker(req, res, next){
+    const password = req.body["password"];
+    if(password === userPassword){
+        userIsAuthorised = true;
+    }
+    next();
+}
+
+app.use(passwordChecker);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+    if(userIsAuthorised){
+        res.sendFile(__dirname + "/public/secret.html");
+    }
+    else{
+        res.sendFile(__dirname + "/public/index.html");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
